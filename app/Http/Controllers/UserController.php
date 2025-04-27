5f1d766047f937295c17f9c7b830408027edf9ef
Fathur <rostafrevolution@gmail.com>
2025-04-27 13:39:05 +0700

    perubahan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // Menggunakan Hash::make untuk meng-hash password
+        $data = [
+            'username' => 'customer-1',
+            'name' => 'Pelanggan Pertama',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
 
+        // Insert data ke dalam tabel
+        UserModel::insert($data);
+
+        // Ambil semua data dari tabel UserModel
+        $user = UserModel::all();
+
+        // Kirim data ke view
         return view('user', ['data' => $user]);
     }
 }
