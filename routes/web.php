95aa363f7930668b2d794ffc9c1b3069322d2e56
Fathur <rostafrevolution@gmail.com>
2025-05-25 14:56:18 +0700

    jobshet 5 part 2

--- routes/web.php
+++ routes/web.php
@@ -3,35 +3,24 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController; // Tambahkan ini
 use Illuminate\Support\Facades\Route;
 
 // Halaman awal
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
-
 Route::get('/kategori', [KategoriController::class, 'index']);
-
 Route::get('/user', [UserController::class, 'index']);
-
 Route::get('/user/tambah', [UserController::class, 'tambah']);
-
 Route::post('user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-// Menampilkan form untuk mengubah data user
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-// Menyimpan perubahan data user
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-
-
 Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
-
-
-
 
 // Route::get('/kategori/update/{id}', [KategoriController::class, 'update']);
 // Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete']);
